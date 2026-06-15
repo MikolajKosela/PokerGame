b133f234abcd352d5e568265b9a63d635fcb3aff
Mikołaj Kosela <mikolaj09521@gmail.com>
2026-06-15 21:21:15 +0200

    Added functions to buttons and emits in frontend

--- static/js/action.js
+++ static/js/action.js
@@ -114,6 +114,47 @@ socket.on("gameData", (data) => {
     }
 })
 
+function changeValue(where, howMuch) {
+  const target = document.getElementById(where);
+  let value = parseInt(target.value) || 1;
+  value += howMuch;
+  if (value < parseInt(target.min)) {
+    value = parseInt(target.min);
+  }
+  target.value = value;
+}
+
+function check() {
+  console.log("check");
+  socket.emit("check");
+}
+
+function bet() {
+  const amount = document.getElementById("betValue");
+  console.log("bet: ", amount.value);
+  socket.emit("bet", {amount: amount.value});
+}
+
+function call() {
+  console.log("call");
+  socket.emit("call");
+}
+
+function raise() {
+  const amount = document.getElementById("raiseValue");
+  console.log("raise: ", amount.value);
+  socket.emit("raise", {amount: amount.value});
+}
+
+function fold() {
+  console.log("fold");
+  socket.emit("fold");
+}
+
+function allin() {
+  console.log("allin");
+  socket.emit("allin");
+}
 /*
       async function whatRoundIs() {
         const res = await fetch("/whoseRound");
