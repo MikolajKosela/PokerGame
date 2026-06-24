f10b45abbfd6d467a98f6b9d4736e3a22e1b9d72
Mikołaj Kosela <mikolaj09521@gmail.com>
2026-06-24 21:34:28 +0200

    Changed way of generating token in handshake (now secrets.token_urlsafe). Some minor changes in handshake()

--- services/auth.py
+++ services/auth.py
@@ -5,42 +5,36 @@ from flask import request
 from flask_socketio import emit
 from app import app, game, socketio 
 
-import random
+from secrets import token_urlsafe
 
 @socketio.on("handshake")
 def handshake(data):
-    ok = False
-    print(data)
-    if check_token(data["token"]) == True:
-        ok = True
+    playerID = check_token(data["token"]) 
 
-    admin = False
+    if playerID == None:
+        return socketio.emit("handshakeAnswer", {"ok" : False}, to=request.sid)
 
-    if ok == True:
-        cur_ID = game.token_to_player[int(data["token"])]
-        game.sid_to_player[request.sid] = cur_ID
-        if cur_ID >= 0:
-            game.players[cur_ID].sid = request.sid
-        if cur_ID == game.adminID:
-            admin = True
+    game.sid_to_player[request.sid] = playerID 
 
-    socketio.emit("handshakeAnswer", {"ok" : ok, "admin" : admin}, to=request.sid)
+    if playerID >= 0 and playerID < len(game.players):
+        game.players[playerID].sid = request.sid
+
+        return socketio.emit("handshakeAnswer", {"ok" : True, "admin" : playerID == game.adminID}, to=request.sid)
+
+    return socketio.emit("handshakeAnswer", {"ok" : False}, to=request.sid)
+
 
 def grant_token():
-    token = random.randint(1000, 100000)
+    token = token_urlsafe(32)
     while token in game.sid_to_player.keys():
-        token = random.randint(1000, 100000)
+        token = token_urlsafe(32)
 
-    game.token_to_player[token] = len(game.sid_to_player)
+    game.token_to_player[str(token)] = len(game.sid_to_player)
     return token
 
 def check_token(token):
     token = str(token)
-    if token.isdecimal() == False:
-        return False
-
-    token = int(token)
 
     if token in game.token_to_player.keys():
-        return True
-    return False 
+        return game.token_to_player[token]
+    return None 
